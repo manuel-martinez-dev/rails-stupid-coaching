70f47358250f0bcf1025b7c23ed58b604a58ba5f
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-11 14:16:24 +0100

    third commit -views implemented with simple styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
+  # :nodoc:
+
   def ask
   end
 
   def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
